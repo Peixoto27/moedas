4c901eaa774e81f9e7d922f5523a2aa4cda3d270
Peixoto27 <danubio457@gmail.com>
2025-07-24 07:18:14 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
-from flask_cors import CORS  # ← Linha 2
+from flask_cors import CORS
 import random
 import os
 
 app = Flask(__name__)
-CORS(app)  # ← Logo depois de criar o app
+CORS(app)
 
 @app.route("/signals")
 def get_signals():
@@ -18,5 +18,5 @@ def get_signals():
     return jsonify(sample)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
